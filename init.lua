bed1c17f238021d15f4fcf4b71509e51e7adcf68
PJDuszynski <pduszynski@trinity-hcr.com>
2024-01-12 12:53:17 -0600

    add some vim opts

--- init.lua
+++ init.lua
@@ -1,5 +1,18 @@
 vim.g.dap_virtual_text = true
 vim.opt.colorcolumn = "140"
 
--- Disable persistent undo
+
+vim.opt.autowrite = true
+vim.opt.cmdheight = 2
+vim.opt.confirm = true
+vim.opt.scrolloff = 8
+vim.opt.swapfile = false
 vim.opt.undofile = false
+
+
+
+vim.filetype.add({
+  extension = {
+    nomad = "hcl",
+  },
+})
