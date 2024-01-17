00c4b918ebc46a7ace2efcd01b93ad20ae7d907a
PJDuszynski <scottduszy@proton.me>
2024-01-16 18:00:37 -0600

    lua formatting

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 vim.g.dap_virtual_text = true
 vim.opt.colorcolumn = "140"
 
-
 vim.opt.autowrite = true
 vim.opt.cmdheight = 2
 vim.opt.confirm = true
@@ -9,12 +8,12 @@ vim.opt.scrolloff = 8
 vim.opt.swapfile = false
 vim.opt.undofile = false
 vim.opt.list = true
-vim.opt.listchars = 'space:·'
+vim.opt.listchars = "space:·"
 
 vim.g.indent_blankline_use_treesitter = true
 
-vim.filetype.add({
+vim.filetype.add {
   extension = {
     nomad = "hcl",
   },
-})
+}
