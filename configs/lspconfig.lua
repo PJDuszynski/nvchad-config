d83b74b761239d83ed29f8711dd06e365c1c82d2
PJDuszynski <scottduszy@proton.me>
2024-01-16 22:14:59 -0600

    add json ls

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "cssls", "clangd", "pyright" }
+local servers = { "cssls", "clangd", "pyright", "jsonls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
