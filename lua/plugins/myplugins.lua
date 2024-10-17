f03b605aed9c61ac97de7943f6732b20a0c15e31
PJDuszynski <pduszynski@trinity-hcr.com>
2024-10-16 20:55:31 -0500

    refactor: add plugins and update lockfile

---
+++ lua/plugins/myplugins.lua
@@ -0,0 +1,143 @@
+-- 1) store all options in separate file in custom/configs/<extension_name>.lusa
+-- 2) Only event, dependencies and lazy should have information differing from the plugin config files
+
+local plugins = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require "configs.treesitter"
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = require("configs.vim-tmux-navigator").cmd,
+    lazy = false,
+  },
+  {
+    "tpope/vim-dadbod",
+  },
+  {
+    "hiphish/rainbow-delimiters.nvim",
+    lazy = false,
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    event = "VeryLazy",
+    config = function()
+      require "configs.venv-selector"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require "nvchad.configs.lspconfig"
+      require "configs.lspconfig"
+      require "configs.null-ls"
+    end,
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function()
+      return require "configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end,
+  },
+  {
+    "NeogitOrg/neogit",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true,
+  },
+  {
+    "junnplus/lsp-setup.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "williamboman/mason.nvim", -- optional
+      "williamboman/mason-lspconfig.nvim", -- optional
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+  },
+  {
+    "nvim-pack/nvim-spectre",
+    event = "VeryLazy",
+  },
+  {
+    "NoahTheDuke/vim-just",
+    ft = { "just" },
+  },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = function()
+      return require("configs.vim-dadbod-ui").cmd
+    end,
+    init = function()
+      require("configs.vim-dadbod-ui").init()
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    event = "VeryLazy",
+    config = function()
+      require "configs.mason-pkgs"
+    end,
+  },
+  {
+    "stevearc/oil.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = function()
+      return require "configs.oil"
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    config = function()
+      return require "configs.noice"
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+  {
+    "Wansmer/sibling-swap.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("sibling-swap").setup()
+    end,
+    event = "VeryLazy",
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require "configs.harpoon"
+    end,
+    event = "VeryLazy",
+  },
+}
+
+return plugins
