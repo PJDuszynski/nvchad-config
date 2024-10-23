79f5b02b778b33d09db7d29bfc9fbd4c1d74ef9a
PJDuszynski <pduszynski@trinity-hcr.com>
2024-10-23 16:49:09 -0500

    remove split mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -38,7 +38,3 @@ map("v", "<F11>", "<cmd>set spell!<CR>", { desc = "Toggle spellcheck" })
 map("v", "C-C", '"+y', { desc = "Copy to system clipboard" })
 map("v", "<F11>", "<cmd>set spell!<CR>", { desc = "Toggle spellcheck" })
 
--- Split Windows
-map("n", "<C-s>v", "<cmd>vsplit<CR>", { desc = "Split Vertically" })
-map("n", "<C-s>h", "<cmd>split<CR>", { desc = "Split Horizontally" })
-map("n", "<C-s>q", "<cmd>close<CR>", { desc = "Close Pane" })
